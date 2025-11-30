872c028a581b2890c8c38862c030690e97031afc
cedrik darel <cedrikdarel0@gmail.com>
2025-11-30 11:22:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-90789557-6d54a",
+  "appId": "1:1021762591288:web:7ed410a67bbc04761de4cd",
+  "apiKey": "AIzaSyDE7eWwB79ByoT32UuxnVseVXAT1XxkCDY",
+  "authDomain": "studio-90789557-6d54a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1021762591288"
+};
